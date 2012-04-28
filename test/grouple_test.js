1b46420f33ebdf94e53f8a201b7f8b90393670fe
Jonathan Lancar <jonaphin@gmail.com>
2012-04-28 08:51:07 -0700

    tests

--- test/grouple_test.js
+++ test/grouple_test.js
@@ -22,7 +22,7 @@
       raises(block, [expected], [message])
   */
 
-  module('jQuery#awesome', {
+  module('jQuery#grouple', {
     setup: function() {
       this.elems = $('#qunit-fixture').children();
     }
@@ -30,28 +30,7 @@
 
   test('is chainable', 1, function() {
     // Not a bad test to run on collection methods.
-    strictEqual(this.elems.awesome(), this.elems, 'should be chaninable');
-  });
-
-  test('is awesome', 1, function() {
-    strictEqual(this.elems.awesome().text(), 'awesomeawesomeawesome', 'should be thoroughly awesome');
-  });
-
-  module('jQuery.awesome');
-
-  test('is awesome', 1, function() {
-    strictEqual($.awesome(), 'awesome', 'should be thoroughly awesome');
-  });
-
-  module(':awesome selector', {
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
-    }
-  });
-
-  test('is awesome', 1, function() {
-    // Use deepEqual & .get() when comparing jQuery objects.
-    deepEqual(this.elems.filter(':awesome').get(), this.elems.last().get(), 'knows awesome when it sees it');
+    strictEqual(this.elems.grouple(), this.elems, 'should be chainable');
   });
 
 }(jQuery));
